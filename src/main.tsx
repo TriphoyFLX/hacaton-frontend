56d01c44525aaad3483c0000c75ce7defe8d9075
Dima <dima20eastern09@gmail.com>
2026-07-18 15:57:23 +0600

    Unregister stale service workers that pin deleted JS bundles

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,16 @@ import './styles/responsive.css'
 import './styles/daw.css'
 import App from './App.tsx'
 
+// Drop stale service workers / cached shells that keep serving deleted JS bundles
+if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+  void navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((reg) => void reg.unregister())
+  })
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
